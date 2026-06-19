27f647f7a12367362c1a99416143347265040cec
Emmanuel Anthony <antorneejnr@gmail.com>
2026-06-19 17:35:20 +0100

    Week 11 Session 2 - render fetched users to DOM

--- week-11/week11.js
+++ week-11/week11.js
@@ -22,14 +22,44 @@
 
 // getMessage();
 
+const createUserCard = (name, email, phone) => {
+  const card = document.createElement("div");
+  card.classList.add("user-card");
+
+  const userName = document.createElement("p");
+  userName.classList.add("user-name");
+  userName.textContent = `${name}`;
+
+  const userMail = document.createElement("p");
+  userMail.classList.add("user-mail");
+  userMail.textContent = `${email}`;
+
+  const userPhone = document.createElement("p");
+  userPhone.classList.add("user-phone");
+  userPhone.textContent = `${phone}`;
+
+  card.appendChild(userName);
+  card.appendChild(userMail);
+  card.appendChild(userPhone);
+
+  return card;
+};
+
 const fetchUser = async () => {
   try {
     const api = await fetch("https://jsonplaceholder.typicode.com/users");
     const apiData = await api.json();
-    console.log(apiData);
+    const renderCard = (data) => {
+      const userContainer = document.getElementById("user-container");
+      userContainer.innerHTML = "";
+      data.forEach((apiData) => {
+        const { name, email, phone } = apiData;
+        userContainer.appendChild(createUserCard(name, email, phone));
+      });
+    };
+    renderCard(apiData);
   } catch (error) {
     console.log(error);
   }
 };
-
 fetchUser();
