d725a814fbde65d77c67d204d8aa01b65fa180c2
Emmanuel Anthony <antorneejnr@gmail.com>
2026-06-26 10:48:58 +0100

    Week 12 Session 2 - render saved users from localStorage

--- week12/week12.js
+++ week12/week12.js
@@ -1,8 +1,41 @@
+let users = [];
+
+const renderUsers = () => {
+  const users = JSON.parse(localStorage.getItem("users")) || [];
+  const renderContainer = document.getElementById("user-list");
+  renderContainer.innerHTML = "";
+  users.forEach((user) => {
+    const nameText = document.createElement("p");
+    nameText.textContent = user.name;
+
+    const emailText = document.createElement("p");
+    emailText.textContent = user.email;
+
+    renderContainer.appendChild(nameText);
+    renderContainer.appendChild(emailText);
+  });
+};
+
 document.getElementById("save-btn").addEventListener("click", () => {
   const nameInput = document.getElementById("name-input");
   localStorage.setItem("name", nameInput.value);
-  console.log(localStorage.getItem("name"));
+  localStorage.getItem("name");
   const emailInput = document.getElementById("email-input");
   localStorage.setItem("email", emailInput.value);
-  console.log(localStorage.getItem("email"));
+  localStorage.getItem("email");
+  users.push({ name: nameInput.value, email: emailInput.value });
+  localStorage.setItem("users", JSON.stringify(users));
+  renderUsers();
+  nameInput.value = "";
+  emailInput.value = "";
 });
+
+//local storage
+// // Save
+// localStorage.setItem("name", "Manny");
+
+// // Read
+// localStorage.getItem("name"); // "Manny"
+
+// // Delete
+// localStorage.removeItem("name");
